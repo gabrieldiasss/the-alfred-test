4a1d2a76d69bf2390f4c1f18f620c7cda4a4d0ac
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-06-13 14:13:56 -0300

    fix: error https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import { hash, publicKey, time } from '@/utils/marvelApi'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://gateway.marvel.com/v1/public',
+  baseURL: 'https://gateway.marvel.com/v1/public',
   params: {
     ts: time,
     apikey: publicKey,
